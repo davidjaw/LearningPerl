99be33cecc1b870867ffb55b9f944d9be3307eeb
David Jaw <jdw.davidjaw@gmail.com>
2015-02-10 17:47:56 +0800

    2015/02/10
    it work!

--- Moose/MoosePractice01_ChatSort.pl
+++ Moose/MoosePractice01_ChatSort.pl
@@ -1,4 +1,4 @@
-use v5.18.2;
+#!/use/bin/perl
 use Data::Dumper;
 package Chat{
   use Moose;
@@ -8,7 +8,7 @@ package Chat{
     => from 'Str'
     => via { 
       my $self = shift;
-      my ($ID, $content) = m'[\S]+ (.+?) (.+)$';
+      my ($ID, $content) = m'[\S]+ (.+?) (.*)$';
       return Chat::ID->new( ID => $ID, content => $content) };
   has 'id' => ( is => 'rw', isa => 'Chat::ID', coerce => 1);
   sub add {
@@ -23,47 +23,48 @@ package Chat::ID{
   my $ID;
   sub BUILD {
     my ($self, $ref) = @_;
+    my $content = '';
+    $content = $1 if $ref->{content} =~/\[\[(.+)/;
+    my $job = '';
+    $job = $content if m'(歌)|(盜)|(傭)|(富)|(豪)|(壕)|(非)';
     if(exists $ID->{$ref->{ID}}){
-      $ID->{$ref->{ID}}->content->addcontent( $ref->{content} );
-      $ID->{$ref->{ID}}->times( $self->times + 1);
+      $self = $ID->{$ref->{ID}};
+      $self->shownTimes( $self->shownTimes + 1);
     }
     else {
       $ID->{$ref->{ID}} = $self;
       push @{$ID->{Refs}}, $self;
-      $self->times( $self->times + 1);
+      $self->shownTimes(1);
     }
+    $self->job($job) if $job ne '';
+    $self->gossip($content) if $content ne '' && $content !~ m'(歌)|(盜)|(傭)|(富)|(豪)|(壕)|(非)';
   }
   has 'ID' => ( is => 'ro', isa => 'Str' );
-  has 'times' => ( is => 'rw', isa => 'Int' );
-  coerce 'Chat::ID::Content' 
-    => from 'Str'
-    => via { Chat::ID::Content->new( content => $_ ) };
-  has 'content' => ( is => 'rw', isa => 'Chat::ID::Content', coerce => 1);
+  has 'shownTimes' => ( is => 'rw', isa => 'Int' );
+  has 'content' => ( is => 'rw', isa => 'Str');
+  has 'job' => ( is => 'rw', isa => 'Str', predicate => 'hasJob');
+  has 'gossip' => ( is => 'rw', isa => 'Str', predicate => 'hasGossip');
   sub sortID {
     return @{$ID->{Refs}};
   }
   1;
 }
-package Chat::ID::Content {
-  use Moose;
-  use Moose::Util::TypeConstraints;
-  has 'content' => ( is => 'rw', isa => 'Str');
-  sub addcontent {
-  
-  }
-  1;
-}
-use Data::Dumper;
+
+use v5.18.2;
 my $agent = Chat->new;
+
 while(<DATA>){
   $agent->add($_);
 }
-for(Chat::ID->sortID){
-  print Dumper $_;
+for my $self(Chat::ID->sortID){
+  say 'ID: ', $self->ID;
+  say 'Job: ', $self->job if $self->hasJob;
+  say 'Gossip: ', $self->gossip if $self->hasGossip;
+  say 'Shwontimes: ', $self->shownTimes;
+  say "";
 }
-# print Dumper $agent;
 __DATA__
-23:21 kenny 紅水要怎麼喝==？
+23:21 kenny 紅水要怎麼喝==？[[asdf
 23:21 張富翔 6C 打一次26K
 23:21 張富翔 去倉庫喝
 23:21 Sih_Yuan ……
@@ -75,7 +76,7 @@ __DATA__
 23:22 kenny 沒看到紅水
 23:22 david 趙 那就是你沒有水
 23:22 張富翔 你是當天送的??
-23:22 david 趙 MENU 下面那牌有個購物車
+23:22 david 趙 MENU 下面那牌有個購物車[[歌姬
 23:22 張富翔 還是用石頭買的
 23:22 kenny 送的
 23:22 E 我開？
